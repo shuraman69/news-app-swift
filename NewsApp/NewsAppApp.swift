06414277b3333c014bfdcc4bce408c0f6e9680be
Alexander Fateev <a.fateev@stayfitt.ru>
2023-02-16 21:50:42 +0300

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Александр Фатеев on 16.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
